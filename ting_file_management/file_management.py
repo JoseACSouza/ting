6d647e2c96a294abf2f3c21a87c5a500f10763d4
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2024-01-09 07:11:02 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file, "r") as file:
+            if not path_file.lower().endswith('.txt'):
+                raise ValueError()
+            file_read = file.read()
+            lines = file_read.split('\n')
+            return lines
+    except FileNotFoundError:
+        sys.stderr.write("Arquivo " + path_file + " não encontrado\n")
+        return None
+    except ValueError:
+        sys.stderr.write("Formato inválido")
+        return None
